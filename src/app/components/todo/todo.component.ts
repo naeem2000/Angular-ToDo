f57b8a5bfa3b431d29938fc418afe7df09d427bc
Naeem Carr <naeem@liquidthought.co.za>
2023-09-09 19:46:59 +0200

    almost finished functionality

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -8,12 +8,26 @@ import { ToDo } from 'src/app/models/todo';
 })
 export class TodoComponent {
   todos!: ToDo[];
+  inputTodo: string = '';
   constructor() {}
 
   ngOnInit(): void {
     this.todos = [
-      { content: 'first content', completed: true },
-      { content: 'second content', completed: true },
+      { id: 1, content: 'first content', completed: false },
+      { id: 2, content: 'second content', completed: false },
+      { id: 3, content: 'third content', completed: false },
     ];
+  }
+
+  toggleDone(id: number) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    });
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
   }
 }
--- src/app/models/todo.ts
+++ src/app/models/todo.ts
@@ -1,4 +1,5 @@
 export class ToDo {
+  id!: number;
   content!: string;
   completed!: boolean;
 }
