a95d0f58333061e598ae76bf568810cc56ce4a92
Naeem Carr <naeem@liquidthought.co.za>
2023-09-09 12:39:27 +0200

    added dummy data

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { ToDo } from 'src/app/models/todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent {}
+export class TodoComponent {
+  todos!: ToDo[];
+  constructor() {}
+
+  ngOnInit(): void {
+    this.todos = [
+      { content: 'first content', completed: true },
+      { content: 'second content', completed: true },
+    ];
+  }
+}
