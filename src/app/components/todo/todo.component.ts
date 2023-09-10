aa78c4e987a1c6dff6223eb2edc61904ac1cb074
Naeem Carr <naeem@liquidthought.co.za>
2023-09-10 19:40:00 +0200

    finished functionality

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,33 +1,52 @@
-import { Component } from '@angular/core';
-import { ToDo } from 'src/app/models/todo';
+import { Component, OnInit } from '@angular/core';
+import { ToDo } from 'src/app/types/todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
   todos!: ToDo[];
   inputTodo: string = '';
+
   constructor() {}
 
   ngOnInit(): void {
-    this.todos = [
-      { id: 1, content: 'first content', completed: false },
-      { id: 2, content: 'second content', completed: false },
-      { id: 3, content: 'third content', completed: false },
-    ];
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      this.todos = JSON.parse(storedTodos);
+    }
+  }
+
+  addTodo() {
+    if (this.inputTodo === '') {
+      console.log('naah');
+    } else {
+      this.todos.push({
+        id: this.todos.length + 1,
+        content: this.inputTodo,
+        completed: false,
+      });
+      this.inputTodo = '';
+      this.saveTodos();
+    }
   }
 
   toggleDone(id: number) {
     this.todos.map((v, i) => {
       if (i == id) v.completed = !v.completed;
-
       return v;
     });
+    this.saveTodos();
   }
 
   deleteTodo(id: number) {
     this.todos = this.todos.filter((v, i) => i !== id);
+    this.saveTodos();
+  }
+
+  saveTodos(): void {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
