fd65c2730d540ab21990d60d0f0ab06be5b43bcf
Naeem Carr <naeem@liquidthought.co.za>
2023-09-19 10:39:17 +0200

    Welcome works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { WelcomeComponent } from './components/welcome/welcome.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
@@ -5,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent],
+  declarations: [AppComponent, TodoComponent, WelcomeComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
